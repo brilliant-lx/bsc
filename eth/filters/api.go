baf8f8cb8a547d303291d5a98474f665cfca3e5f
Uri-Margalit-bloXroute <uri.margarlit@bloxroute.com>
2023-06-20 18:11:03 +0300

    miner: adding MEVRelay log

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -557,7 +557,7 @@ func (api *PublicFilterAPI) GetFilterChanges(id rpc.ID) (interface{}, error) {
 		f.deadline.Reset(api.timeout)
 
 		switch f.typ {
-		case BlocksSubscription, FinalizedHeadersSubscription, VotesSubscription:
+		case PendingTransactionsSubscription, BlocksSubscription, FinalizedHeadersSubscription, VotesSubscription:
 			hashes := f.hashes
 			f.hashes = nil
 			return returnHashes(hashes), nil
